81262a02cc26e2a11477db7facae0013ee629378
JohnLRanola <johnlranola@gmail,com>
2023-10-15 19:02:40 +0100

    Added new logic for cipher WORKING now

--- vigenere.c
+++ vigenere.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-void removeLine(char *str) {
+void removeLines(char *str) {
     int length = strlen(str);
     if (str[length - 1] == '\n') {
         str[length - 1] = '\0';
@@ -9,6 +9,25 @@ void removeLine(char *str) {
 }
 
 void vigenereEncrypt(char input[], char keyword[]) {
+
+    int inputLength = strlen(input);
+    int keywordLength = strlen(keyword);
+    int i, j;
+
+    for (i = 0, j = 0; i < inputLength; ++i) {
+
+        char curChar = input[i];
+
+         if ((curChar >= 'a' && curChar <= 'z') || (curChar >= 'A' && curChar <= 'Z')) {
+            char base = (curChar >= 'a' && curChar <= 'z') ? 'a' : 'A';
+            input[i] = (curChar + keyword[j] - 2 * base) % 26 + base;
+            ++j;
+
+            if (j == keywordLength) {
+                j = 0; 
+            }
+         }
+    }
 }
 
 int main() {
