b624ffa9758a27ab6435ec133046ddf76bc15be9
JohnLRanola <johnlranola@gmail,com>
2023-10-15 20:03:12 +0100

    Added comments

--- vigenere.c
+++ vigenere.c
@@ -13,13 +13,36 @@ void vigenereEncrypt(char input[], char keyword[]) {
     int keywordLength = strlen(keyword);
     int i, j = 0;
 
+    // Loops through each character of input
     for (i = 0; i < inputLength; ++i) {
         char curChar = input[i];
 
+    // This checks if character is in the alphabet
         if ((curChar >= 'a' && curChar <= 'z') || (curChar >= 'A' && curChar <= 'Z')) {
+    
+    // Converts characters to uppercase or lowercase when needed
+    // keywordChar stores the character 
             char base = (curChar >= 'a' && curChar <= 'z') ? 'a' : 'A';
             char keywordChar = keyword[j];
 
+    /*
+    The basis of this cipher is the formula which is (curChar
+    - base + keywordChar - 'a' ) % 26 + base;
+
+    curChar - base converts the current character to a number between
+    0 - 25 depending on the position they are at in the alphabet
+
+    keywordChar - 'a' is used to convert the keyword character to a
+    number value
+
+    (curChar - base + keywordChar - 'a') % 26 adds two values 
+    and takes the result modulo 26. This makes sure it is in the
+    number of alphabet
+
+    The + base then converts the number back to character in the 
+    right case and puts it in input[i]
+
+    */
             input[i] = (curChar - base + keywordChar - 'a') % 26 + base;
             ++j;
 
