da33a2ef8b7d8dad490ddb8b92294f1ae9d4db5b
JohnLRanola <johnlranola@gmail,com>
2023-10-15 21:34:27 +0100

    Modified the menu

--- Question6/hillCipher.c
+++ Question6/hillCipher.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
+
+
 int main() {
     char input[100];
     char keyword[5];
 
-    printf("Enter TEXT in UPPERCASE: ");
+    printf("Enter TEXT: ");
     scanf("%s", input);
     
-    printf("Enter a 4 letter KEYWORD in UPPERCASE: ");
+    printf("Enter a 4 letter KEYWORD: ");
     scanf("%s", keyword);
+
+    if (strlen(keyword) != 4) {
+        printf("ERROR. Please enter a 4 LETTER KEYWORD\n");
+        return 1;
+    }
+
+    printf("Encrypted Text: ");
+    hillCipher(input, keyword);
+    printf("\n");
+
+    return 0;
 }
