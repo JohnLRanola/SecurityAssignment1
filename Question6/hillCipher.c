b7f469f14f474da667446e01ff57e8a02b215ea0
JohnLRanola <johnlranola@gmail,com>
2023-10-15 21:36:42 +0100

    Added a modInverse function with appropriate logic

--- Question6/hillCipher.c
+++ Question6/hillCipher.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
+int modInverse(int a, int m) {
+    a = a % m;
 
+    for (int x = 1; x < m; x++) {
+        if ((a * x) % m == 1) {
+            return x;
+        }
+    }
+}
 
 int main() {
     char input[100];
