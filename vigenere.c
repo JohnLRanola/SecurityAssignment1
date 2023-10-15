3e9ccda42851c856294a01db0dbb3b2ac3486325
JohnLRanola <johnlranola@gmail,com>
2023-10-15 18:33:22 +0100

    Added Uppercase for function

--- vigenere.c
+++ vigenere.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
+void vigenereEncrypt(char input[], char keyword[]) {
+
+    int inputLength = strlen(input);
+    int keywordLength = strlen(keyword);
+    char enryptedText[inputLength];
+
+    for (int i = 0; i < inputLength; i++) {
+
+        char inputChar = input[i];
+        char keywordChar = keyword[i % keywordLength];
+
+        // Encryption for uppercase
+        if (inputChar >= 'A' && inputChar <= 'Z') {
+            enryptedText[i] = ((inputChar - 'A' + keywordChar - 'A') % 26);
+        }
+    }
+
+
+}
+
 int main() {
 
     char input[100];
